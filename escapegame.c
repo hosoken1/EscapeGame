202c645275968e5a5dc05ac0017aa1d6d59306f7
DESKTOP-S7QCKNN\hosok <hosoken1edm@gmail.com>
2022-07-29 20:14:13 +0900

    プレイヤーが！動ける！動けるぅ！

--- escapegame.c
+++ escapegame.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define MAP_Length 10
+#define MAP_Length 10//マップの大きさ
+#define X 1
+#define Y 0
 void MAP_init();
 void MAP_print();
+void Player_Move(char);
 // マップの情報　０が壁　１が通れるところ　
 // ２がプレイヤー　３がゴール
 int MAP[10][10] = {{1,2,1,0,1,0,1,1,1,1},
@@ -15,19 +18,20 @@ int MAP[10][10] = {{1,2,1,0,1,0,1,1,1,1},
                    {0,1,0,0,0,1,0,0,1,0},
                    {1,1,1,1,1,1,1,0,1,0},
                    {0,1,0,1,0,0,0,0,1,3}};
+int PlayerTrans[2] = {0,1};//プレイヤーの位置を表す（y,x）
 int main(void){
     char input;
     while(1){
-        //system("cls");//画面のクリア
         MAP_print();//マップの描画
-        scanf("%c",&input);
+        scanf(" %c",&input);
         if(input == 'q')break;
+        Player_Move(input);
     }
     return 0;
 }
 
 void MAP_print(){
-    //system("cls");
+    system("cls");
     for(int i=0;i<MAP_Length;i++){
         for(int j=0;j<MAP_Length;j++){
             if(MAP[i][j] == 0)
@@ -46,4 +50,21 @@ void MAP_print(){
         }
         printf("\n");
     }
+}
+
+void Player_Move(char input){
+    MAP[PlayerTrans[Y]][PlayerTrans[X]] = 1;//プレイヤーのいた位置を１に初期化する
+    if(input == 'w'){
+        if(PlayerTrans[Y]-1 >= 0 && MAP[PlayerTrans[Y]-1][PlayerTrans[X]] != 0)PlayerTrans[Y]-=1;
+    }
+    else if(input == 's'){
+        if(PlayerTrans[Y]+1 < MAP_Length && MAP[PlayerTrans[Y]+1][PlayerTrans[X]] != 0)PlayerTrans[Y]+=1;
+    }
+    else if(input == 'a'){
+        if(PlayerTrans[X]-1 >= 0 && MAP[PlayerTrans[Y]][PlayerTrans[X]-1] != 0)PlayerTrans[X]-=1;
+    }
+    else if(input == 'd'){
+        if(PlayerTrans[X]+1 >= 0 && MAP[PlayerTrans[Y]][PlayerTrans[X]+1] != 0)PlayerTrans[X]+=1;
+    }
+    MAP[PlayerTrans[Y]][PlayerTrans[X]] = 2;
 }
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,3 @@
+else if(input == 'a'){
+    //     if(PlayerTrans[0]-1 >= 0 && MAP[PlayerTrans[1]][PlayerTrans[0]-1] != 0)PlayerTrans[1]-=1;
+    // }
