c65aac4bc1e858a69fe0ffc59977f76a555f880f
DESKTOP-S7QCKNN\hosok <hosoken1edm@gmail.com>
2022-07-31 21:22:57 +0900

    マップの不可視化の処理完成

--- escapegamever2.c
+++ escapegamever2.c
@@ -9,24 +9,36 @@ void MAP_print();
 void Player_Move(char);
 // マップの情報　０が壁　１が通れるところ　
 // ２がプレイヤー　３がゴール
-int MAP[10][10] = {{1,2,1,0,1,0,1,1,1,1},
-                   {0,1,0,0,1,1,0,1,0,1},
-                   {1,1,0,0,1,0,0,1,0,0},
-                   {0,1,1,1,1,1,1,1,1,1},
-                   {0,1,0,0,0,0,1,0,0,0},
-                   {1,1,0,1,0,0,1,0,0,1},
-                   {0,1,1,1,1,1,1,1,1,1},
-                   {0,1,0,0,0,1,0,0,1,0},
-                   {1,1,1,1,1,1,1,0,1,0},
-                   {0,1,0,1,0,0,0,0,1,3}};
-int PlayerTrans[2] = {0,1};//プレイヤーの位置を表す（y,x）
-int GoalTrans[2] = {9,9};//ゴールの位置を表す
+int MAP[MAP_Length][MAP_Length]  = {{0,0,0,0,0,0,0,0,0,0},
+                                    {0,2,0,0,1,1,0,1,0,0},
+                                    {0,1,0,0,1,0,0,1,0,0},
+                                    {0,1,1,1,1,1,1,1,1,0},
+                                    {0,1,0,0,0,0,1,0,0,0},
+                                    {0,1,0,1,0,0,1,0,0,0},
+                                    {0,1,1,1,1,1,1,1,1,0},
+                                    {0,1,0,0,0,1,0,0,1,0},
+                                    {0,1,1,1,1,1,1,0,1,3},
+                                    {0,0,0,0,0,0,0,0,0,0}};
+//マップの可視化情報　０：不可視　１：可視
+int MAP_visible[MAP_Length][MAP_Length] =  {{0,0,0,0,0,0,0,0,0,0},
+                                            {0,0,0,0,0,0,0,0,0,0},
+                                            {0,0,0,0,0,0,0,0,0,0},
+                                            {0,0,0,0,0,0,0,0,0,0},
+                                            {0,0,0,0,0,0,0,0,0,0},
+                                            {0,0,0,0,0,0,0,0,0,0},
+                                            {0,0,0,0,0,0,0,0,0,0},
+                                            {0,0,0,0,0,0,0,0,0,0},
+                                            {0,0,0,0,0,0,0,0,0,0},
+                                            {0,0,0,0,0,0,0,0,0,0}};
+int PlayerTrans[2] = {1,1};//プレイヤーの位置を表す（y,x）
+int GoalTrans[2] = {8,9};//ゴールの位置を表す
 int main(void){
     char input;
+    MAP_init();
     while(1){
         MAP_print();//マップの描画
         if(PlayerTrans[X] == GoalTrans[X] && PlayerTrans[Y] == GoalTrans[Y]){
-            printf("ゴールしました！");
+            printf("脱出成功！");
             break;
         }
         scanf(" %c",&input);
@@ -39,17 +51,23 @@ int main(void){
     Sleep(3000);
     return 0;
 }
+void MAP_init(){
+    //プレイヤーを可視化
+    MAP_visible[PlayerTrans[Y]][PlayerTrans[X]] = 1;
+    MAP_visible[GoalTrans[Y]][GoalTrans[X]] = 1;
+}
 
 void MAP_print(){
     system("cls");
+    printf("脱出口を目指せ！\nプレイヤー:◎　脱出口:◆\n");
     for(int i=0;i<MAP_Length;i++){
         for(int j=0;j<MAP_Length;j++){
-            if(MAP[i][j] == 0)
+            if(MAP[i][j] == 1 || MAP_visible[i][j] == 0)
             {
-                printf("■");
-            }
-            else if(MAP[i][j] == 1){
                 printf("　");
+            }
+            else if(MAP[i][j] == 0){
+                printf("■");
             }
             else if(MAP[i][j] == 2){
                 printf("◎");
@@ -60,21 +78,39 @@ void MAP_print(){
         }
         printf("\n");
     }
+    printf("w a s d + Enterで移動、qで終了\n");
 }
 
 void Player_Move(char input){
     MAP[PlayerTrans[Y]][PlayerTrans[X]] = 1;//プレイヤーのいた位置を１に初期化する
     if(input == 'w'){
-        if(PlayerTrans[Y]-1 >= 0 && MAP[PlayerTrans[Y]-1][PlayerTrans[X]] != 0)PlayerTrans[Y]-=1;
+        MAP_visible[PlayerTrans[Y]-1][PlayerTrans[X]] = 1;
+        if(PlayerTrans[Y]-1 >= 0 && MAP[PlayerTrans[Y]-1][PlayerTrans[X]] != 0)
+        {
+            PlayerTrans[Y]-=1;
+        }
     }
     else if(input == 's'){
-        if(PlayerTrans[Y]+1 < MAP_Length && MAP[PlayerTrans[Y]+1][PlayerTrans[X]] != 0)PlayerTrans[Y]+=1;
+        MAP_visible[PlayerTrans[Y]+1][PlayerTrans[X]] = 1;
+        if(PlayerTrans[Y]+1 < MAP_Length && MAP[PlayerTrans[Y]+1][PlayerTrans[X]] != 0)
+        {
+            PlayerTrans[Y]+=1;
+        }
     }
     else if(input == 'a'){
-        if(PlayerTrans[X]-1 >= 0 && MAP[PlayerTrans[Y]][PlayerTrans[X]-1] != 0)PlayerTrans[X]-=1;
+        MAP_visible[PlayerTrans[Y]][PlayerTrans[X]-1] = 1;
+        if(PlayerTrans[X]-1 >= 0 && MAP[PlayerTrans[Y]][PlayerTrans[X]-1] != 0)
+        {
+            PlayerTrans[X]-=1;
+        }
     }
     else if(input == 'd'){
-        if(PlayerTrans[X]+1 >= 0 && MAP[PlayerTrans[Y]][PlayerTrans[X]+1] != 0)PlayerTrans[X]+=1;
+        MAP_visible[PlayerTrans[Y]][PlayerTrans[X]+1] = 1;
+        if(PlayerTrans[X]+1 >= 0 && MAP[PlayerTrans[Y]][PlayerTrans[X]+1] != 0)
+        {
+            PlayerTrans[X]+=1;
+        }
     }
     MAP[PlayerTrans[Y]][PlayerTrans[X]] = 2;
+    //MAP_visible[PlayerTrans[Y]][PlayerTrans[X]] = 1;
 }
